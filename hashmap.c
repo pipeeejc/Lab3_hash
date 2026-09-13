7ba76526fbe37599390559bc377384a92ba26cc9
Your Name <you@example.com>
2026-09-13 19:26:18 +0000

    new try

--- hashmap.c
+++ hashmap.c
@@ -181,7 +181,21 @@ Pair * nextMap(HashMap * map) {
 void enlarge(HashMap * map) {
     enlarge_called = 1; //no borrar (testing purposes)
 
+    if(map == NULL) return;
 
+    Pair ** old_buckets = map->buckets;
+    long old_capacity = map->capacity;
+
+    map->capacity += 2;
+    map->buckets = (Pair **)calloc(map->capacity ,sizeof(Pair *));
+    map->size = = 0;
+
+    for(long i = 0; i < old_capacity; i++) {
+        if(old_buckets[i] != NULL && old_buckets[i]->key != NULL) {
+            insertMap(map, old_buckets[i]->key, old_buckets[i]->value);
+        }
+    }
+    free(old_buckets);
 }
 
 
